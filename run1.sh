8026f261c42fadcc44e3f194de628d3ec6d6c4db
Tim Jayne <tjwillcode@gmail.com>
2020-05-31 10:13:36 -0400

    05/31/2020_10:13am_commit 1

--- run1.sh
+++ run1.sh
@@ -1,1 +1,1 @@
-gunicorn server1:app --bind 127.0.0.1:8888
+gunicorn server1:app --bind 127.0.0.1:8000
--- server1.py
+++ server1.py
@@ -4,35 +4,32 @@ import json
 
 app = Flask(__name__) #create the Flask app
 
-#http://694d334b.ngrok.io -> http://localhost:8888
-@app.route('/', methods=['GET'])
-def hello_world():
-    return "hello"
-
-
-
-#http://3e014fff.ngrok.io/zap-sent -> http://localhost:8888/zap-sent
+#http://884549d31ca6.ngrok.io/zap-sent -> http://localhost:8080/zap-sent
 @app.route('/zap-sent', methods=['POST','GET'])
 def zap_sent():
-    #return "test"
-    
-    req_data = request.get_json()
-    print(json.dumps(req_data))
-
-    language = req_data ['language']
-    framework = req_data ['framework']
-    python_version = req_data ['version_info']['python']
-    examples = req_data ['examples'][0]
-    boolean_test = req_data ['boolean_test']
-
-
-    return '''<h1>
-        The language value is {}.
-        The framework value is {}.
-        The Python version is {}.
-        The example at 0 index is{}.
-        The boolean value is {}.
-        </h1>'''.format(language, framework, python_version, examples, boolean_test)
+    if request.method == 'POST':
+        print(request.json)
+        return '', 200
+    else:
+        abort(400)
 
 if __name__ == '__main_':
-    app.run(port=8888) #run app on port 8888
+    app.run(port=8000) #run app on port 8080
+
+    #req_data = request.get_json()
+    #print(json.dumps(req_data))
+
+    #language = req_data ['language']
+    #framework = req_data ['framework']
+    #python_version = req_data ['version_info']['python']
+    #examples = req_data ['examples'][0]
+    #boolean_test = req_data ['boolean_test']
+
+
+    #return '''<h1>
+        #The language value is {}.
+        #The framework value is {}.
+        #The Python version is {}.
+        #The example at 0 index is{}.
+        #The boolean value is {}.
+        #</h1>'''.format(language, framework, python_version, examples, boolean_test)
